22751e1f4abed068ce0a7b57a9658406f08d5387
Sinda <sindajeddey@gmail.com>
2021-09-25 00:18:54 +0100

    Finishing work experience

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 import NavBar from './navbar/navbar';
 import Intro from "./intro/intro";
 import AboutMe from "./about-me/about-me";
+import WorkExperience from "./work-experience/work-exp";
+import {createMuiTheme} from "@material-ui/core";
+import {ThemeProvider} from "@mui/material";
 
 
 function App() {
+    const theme = createMuiTheme({palette: {
+            warning: {
+                main: '#E9322E',
+            },
+        },
+        typography: {
+            fontFamily: [
+                'IBM Plex Mono'
+            ].join(','),
+        },});
   return (
-    <div className="App">
-        <NavBar/>
-        <Intro/>
-        <AboutMe/>
-    </div>
+      <ThemeProvider theme={theme}>
+          <div className="App">
+              <NavBar/>
+              <Intro/>
+              <AboutMe/>
+              <WorkExperience/>
+          </div>
+      </ThemeProvider>
+
   );
 }
 
---
+++ src/work-experience/work-exp.js
@@ -0,0 +1,65 @@
+import React from "react";
+import './work-exp.css';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import {Typography} from "@mui/material";
+
+const WorkExperience = () => {
+    return(
+        <div className="container" id="#work-experience">
+            <div className="row mt-5">
+                <div className="col-6 px-5 title">Work Experience</div>
+                <div className="col-6 bar mt-auto mb-auto"/>
+            </div>
+            <div className="row">
+                <Timeline position='alternate'>
+                        <TimelineItem>
+                            <TimelineSeparator>
+                                <TimelineDot color="warning"/>
+                                <TimelineConnector/>
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Typography variant="h6" component="span" style={{'color': "white"}}>
+                                    PetaResearch | Jul. 2021 – Aug. 2021, Tunisia
+                                </Typography>
+                                <Typography style={{'color': "#E5E5E5"}}>
+                                    <ul>
+                                        <li>Implemented Infrasturcture as Code - IaC to manage, track and maintain deployed AWS services.</li>
+                                        <li>Wrote Python scripts for data connectivity to the deployed databases and data streaming services in AWS.</li>
+                                        <li>Implemented multi-threading using Python for data connectivity to AWS Kinesis Streams reaching 100% in read throughput for maximum usage of the data streams.</li>
+                                        <li>Implemented auto-scaling mechanism for AWS Kinesis Stream to scale up or down depending on throughput using AWS CloudWatch alarms.</li>
+                                        <li>Designed and deployed a serverless ETL pipeline that consumes user input.</li>
+                                    </ul>
+                                </Typography>
+                            </TimelineContent>
+                            </TimelineItem>
+                        <TimelineItem>
+                            <TimelineSeparator>
+                                <TimelineDot color="warning"/>
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Typography variant="h6" component="span" style={{'color': "white"}}>
+                                    iPrecision | Jul. 2020 – Sept. 2020, Tunisia
+                                </Typography>
+                                <Typography style={{'color': "#E5E5E5"}}>
+                                    <ul>
+                                        <li>Participated in every step of the product development process of a system that monitors and controls electronic shelf labels for supermarkets from design through development and improvement of features.</li>
+                                        <li>Put together RESTful APIs for the back-end that served data to the React front-end.</li>
+                                        <li>Conducted unit and integration testing on the back-end’s RESTful APIs with 60% code coverage.</li>
+                                        <li>Set up a continuous integration and continuous deployment CI/CD pipeline and enabled e-mail notifications of build results and output logs.</li>
+                                    </ul>
+                                </Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                </Timeline>
+            </div>
+        </div>
+    )
+}
+
+export default WorkExperience
