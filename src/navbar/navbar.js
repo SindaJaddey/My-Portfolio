891cb204a4781575dd454a792ff737e5755983cf
Sinda <sindajeddey@gmail.com>
2021-09-29 16:19:05 +0100

    Refactoring navbar

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
-import "./navbar.css"
+import "./navbar.css";
+
 const NavBar = () => {
     //button on click function to redirect to pdf
     return(
         <>
-            <div className="container mt-3">
-                <nav className="navbar fixed-top">
-                    <div className={"col"}><a href="#about-me" className="navbar-element">About Me</a></div>
-                    <div className={"col"}><a href="#work-experience" className="navbar-element">Work Experience</a> </div>
-                    <div className={"col"}><a href="#projects" className="navbar-element">Projects</a></div>
-                    <div className={"col"}><a href="#contact-me" className="navbar-element">Contact Me</a></div>
-                    <div className={"col"}><a href="#contact-me" className="navbar-element">Resume</a></div>
-                </nav>
+            <div className="container mx-5">
+                <div className="row navBar fixed-top mx-auto py-3">
+                        <div className={"col"}><a href="#about-me" className="navbar-element">About Me</a></div>
+                        <div className={"col"}><a href="#work-experience" className="navbar-element">Work Experience</a> </div>
+                        <div className={"col"}><a href="#projects" className="navbar-element">Projects</a></div>
+                        <div className={"col"}><a href="#contact-me" className="navbar-element">Contact Me</a></div>
+                        <div className={"col"}><a href="#contact-me" className="navbar-element">Resume</a></div>
+                </div>
             </div>
         </>
     )
